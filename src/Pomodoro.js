02a05a8434bd4393a0cb8a4993802c4b57c6876a
alexmccorkle <114952775+alexmccorkle@users.noreply.github.com>
2024-02-12 20:40:56 +0100

    Initializing the project

---
+++ src/Pomodoro.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Pomodoro Timer component
+// This component will be used to create a timer for the Pomodoro Technique
+
+class Pomodoro extends React.Component {
+  render() { 
+    return (
+      <div>
+        <h1>Pomodoro</h1>
+      </div>
+    );
+  }
+}
+
+export default Pomodoro; // Export the Pomodoro component to be used in other files
